12fb7ef88774f24870affb6924118c13eb2d1ceb
Alejandro Martin Garcia <alejandromartingarcia@Ordenador-portatil-de-Alejandro.local>
2024-09-30 12:14:36 +0200

    clases de dominio v1

---
+++ src/dominio/Habitante.java
@@ -0,0 +1,18 @@
+package dominio;
+public class Habitante{
+	privateString nombre;
+	private String apellido1;
+	private String apellido2;
+	public Habitante(){};
+	public Habitaante(String nombre_, String apellido1_, String apellido2_)(
+		nombre=nombre_;
+		apellido=apellido_;{
+	}
+	public String  getNombre(){
+		return nombre;
+	}
+	public void setnNombre(String nombre_{
+		nombre=nombre_;
+	}
+	public String toString(){
+		return "nombre: "nombre+"apellido1+" "apellido2;
