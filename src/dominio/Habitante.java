9af9b3123e42987b01d1f904f75bbddab1674ef9
Alejandro Martín García <alejandromartingarcia@Ordenador-portatil-de-Alejandro-2.local>
2024-10-17 09:46:44 +0200

    datepadron

--- src/dominio/Habitante.java
+++ src/dominio/Habitante.java
@@ -1,18 +1,24 @@
-package dominio;
+package dominio; 
+
 public class Habitante{
-	privateString nombre;
-	private String apellido1;
-	private String apellido2;
-	public Habitante(){};
-	public Habitaante(String nombre_, String apellido1_, String apellido2_)(
+	private String nombre; 
+	private String apellido1; 
+	private String apellido2; 	
+	public Habitante (){};
+	public Habitante(String nombre_, String apellido1_, String apellido2_){
 		nombre=nombre_;
-		apellido=apellido_;{
+		apellido1=apellido1_;
+		apellido2=apellido2_;
 	}
-	public String  getNombre(){
+	public String getNombre(){
 		return nombre;
 	}
-	public void setnNombre(String nombre_{
+	public void setNombre(String nombre_){
 		nombre=nombre_;
 	}
-	public String toString(){
-		return "nombre: "nombre+"apellido1+" "apellido2;
+
+	public String toString (){
+		return "nombre: "+nombre+" "+apellido1+" "+apellido2;
+	
+	}
+	}
